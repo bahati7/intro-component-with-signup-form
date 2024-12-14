eb9878172bb167f97caf35a90d70951af1250a10
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-12-14 10:15:52 +0200

    added reame

--- src/Form.jsx
+++ src/Form.jsx
@@ -18,8 +18,8 @@ const Form = () => {
   return (
     <div>
       <section id="hero">
-        <div className="container flex flex-col md:flex-row px-24 mx-auto mt-10 space-y-0 md:space-y-0">
-          <div className="flex flex-col mb-16  justify-center  space-y-12 md:w-1/2 text-white">
+        <div className="container flex flex-col md:flex-row md:px-24 px-auto justify-center items-center mx-auto mt-10 space-y-0 md:space-y-0">
+          <div className="flex flex-col mb-16  justify-center  space-y-12 md:w-1/2 w-80 text-white">
             <h1
               className="max-w-md text-4xl font-bold text-center
              md:text-5xl md:text-left"
@@ -37,7 +37,7 @@ const Form = () => {
             </p>
           </div>
 
-          <div className="md:w-1/2 flex flex-col items-center p-8">
+          <div className="md:w-1/2 w-80 flex flex-col items-center md:p-8 p-2">
             <button
               type="submit"
               className="max-w-lg w-full mx-auto shadow-lg text-white bg-Blue hover:bg-blue-800 font-medium rounded-lg px-10 py-2.5 text-center"
